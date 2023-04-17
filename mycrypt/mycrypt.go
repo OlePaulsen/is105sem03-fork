0a2ac38ac05adc1efba6a3c31eaa4918e606b36d
OlePaulsen <112427736+OlePaulsen@users.noreply.github.com>
2023-04-17 14:02:03 +0200

    hei

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,16 +1,13 @@
 package mycrypt
 
-var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
+var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ0123456789.,:; ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
-			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
-		} else {
-			kryptertMelding[i] = alphabet[indeks+chiffer]
-		}
+		newIndex := (indeks + chiffer) % len(alphabet)
+		kryptertMelding[i] = alphabet[newIndex]
 	}
 	return kryptertMelding
 }
